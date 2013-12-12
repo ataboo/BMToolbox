4b0e4c3eccba10afbe37671d05cdf820bb6493b1
ataboo <ataboo2001@hotmail.com>
2013-12-12 10:36:31 -0700

    Got some stuff working.

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,1 +1,31 @@
-
+$(document).ready(function(){
+	$.getJSON("data/FlangeArrays.json").done(
+		function(data){
+			console.log(data);
+			console.log(data.fSizes[0]);
+			$('#text1').text(data.fSizes[0]);
+			fSizeTown = data.fSizes;
+			var options = $("#options");
+			$.each(fSizeTown, function(i, value) {
+    			$(options).append($('<option>').text(value).attr('value', value));
+			});
+		});
+	
+	$("#btn1").click(
+		function(){
+			
+    		//$("#text1").text(flangeTest);.
+    		alert("banana");
+    		console.log("banana");
+    	}
+    );
+    
+	$("#btn2").click(
+  			function(){
+    			$("#text2").html("<b>Hello world!</b>");
+    		}
+	);
+	
+	$("#options").change(function(event, changeHandler){
+	});
+});
