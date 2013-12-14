13d7bffb445cc0d74ddfdaa9f24d974f4b5ba049
ataboo <ataboo2001@hotmail.com>
2013-12-14 08:24:39 -0700

    Added everything to context based function generation (compartmentalized).

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,37 +1,46 @@
 
-var BMBox = {};
+var flangeBox = {};  //Used for context to coordinate between the functions.
 
+/*This parses a JSON file with values for ASME flange tables, populates selectors accordingly
+ * and adds listeners to detect value changes and the push of the submit buttons.  If the
+ * selects are changed it enables the submit buttons and outputs the proper values in an
+ * alert box.  It was designed using Jquery Mobile and Cordova.*/
 (function(context) {	
 
-	context.loadJSON = function() {
+	context.startLoad = function() {
 		$("#flange-send").click(function(){
-			calcFlange();
+			context.calcFlange();
 		});
 	
 		$("#stud-send").click(function(){
-			calcStud();
+			context.calcStud();
 		});
+		
+		context.flangeSelClass = $(".flange-sel");
+		
+		context.sizeSel = $("#select-flange-size");
+		context.rateSel = $("#select-flange-rating");
+		context.studSel = $("#stud-select");
+		
 		
 		$.getJSON("data/FlangeArrays.json").done(function(data){
 			context.postLoad(data);
 		});
-		
-
 	};
-		
+	
+	//after the JSON is done loading this is called to run the rest of the setup.	
 	context.postLoad = function(data) {
 		context.data = data;
 		context.popSelects();
-		
+		context.addSelectChange();
 		
 	};
 	
 	context.popSelects = function() {
 		data = context.data;
-		context.sizeSel = selectSize = $("#select-flange-size");
-		context.rateSel = selectRate = $("#select-flange-rating");
-		
-		context.studSel = selectStud = $("#stud-select");
+		selectSize = context.sizeSel;
+		selectRate = context.rateSel;
+		selectStud = context.studSel;
 		
 		$.each(data.fSizes, function(i, value) {
 	   		selectSize.append($('<option>').text(value + "\"").attr('value', value));
@@ -59,137 +68,122 @@ var BMBox = {};
 		context.studBut = $("#stud-send");
 		context.studBut.button("disable");
 		context.studBut.button("refresh");
-		addSelectChange();	
+		context.addSelectChange();	
 	};
-})(BMBox);
-
-$(document).ready(function(){
-	BMBox.loadJSON();	
-});
-
-function popSelects(data){
-
-}
 	
-function addSelectChange(){ 
-	$(".flange-sel").bind('change', function(){
-		console.log("change");
-		updateSendButton();
-	});
-}
+	context.addSelectChange = function() {
+		context.flangeSelClass.bind('change', function(){
+			console.log("change");
+			context.updateSendButton();
+		});
+	};
 	
-function updateSendButton() {
-	console.log("Rate Index:" + BMBox.rateSel[0].selectedIndex);
-	console.log("Size Index: " + BMBox.sizeSel[0].selectedIndex);
-	console.log(BMBox.rateSel[0].selectedIndex > 0);
-	
-	if(BMBox.rateSel[0].selectedIndex > 0 &&
-		BMBox.sizeSel[0].selectedIndex > 0) {
-		BMBox.sendBut.button("enable");
-	} else {
-		BMBox.sendBut.button("disable");
-	}
-	BMBox.sendBut.button("refresh");
-	
-	if(BMBox.studSel[0].selectedIndex > 0) {
-		BMBox.studBut.button("enable");
-	} else {
-		BMBox.studBut.button("disable");
-	}
-	BMBox.studBut.button("refresh");
-}
+	context.updateSendButton = function () {
 
-function calcStud() {
-	console.log("ran calc stud");
-	studVal = BMBox.studSel[0].selectedIndex;	
-	console.log("studval: " + studVal);
-	getStudStatsOrdered(studVal - 1);
-	displayStud();
-}
-
-
-function getStudStatsOrdered(studSize){
-	BMBox.studStats = BMBox.studStatsOrdered[studSize][0];
-	console.log(BMBox.studStats);
-}
-	
-function displayStud(){
-	studStats = BMBox.studStats;
-	studSize = BMBox.studSel.val();
-	studString = ("Stud Size: " + studSize + "\"");
-	toolString = ("Wrench: " + studStats[0] + "\",   Drift Pin: " + studStats[1] + "\"");
-	torqueString = ("B7 Torque: " + studStats[3] + " ft-lbs \nB7M Torque: " + studStats[2] + " ft-lbs");
-	
-	outString = (studString + "\n" +
-		toolString + "\n" +
-		torqueString);
-	
-	alert(outString);
-	
-}
-
-function calcFlange() {
-	sSize = BMBox.sizeSel;
-	sRate = BMBox.rateSel;
-
-	rateInd = sRate[0].selectedIndex;
-	BMBox.rateVal = sRate.val();
-	BMBox.sizeVal = sizeVal = $(sSize).val();
-	
-	//Inside the $.each() callback above, you would do $.each(this.subaction, function() { alert(this.name); });, that would give you A, B, C, etc. - test it out here:
-
-	switch(rateInd) {
-		case 0:
-			getFStats(BMBox.data.fStats150, sizeVal);
-			break;
-        case 1:
-			getFStats(BMBox.data.fStats300, sizeVal);
-			break;
-		case 2:
-			getFStats(BMBox.data.fStats400, sizeVal);
-			break;
-	    case 3:
-			getFStats(BMBox.data.fStats600, sizeVal);
-			break;
-	    case 4:
-			getFStats(BMBox.data.fStats900, sizeVal);
-			break;
-		case 5:
-			getFStats(BMBox.data.fStats1500, sizeVal);
-			break;
-	}		
-}
-			
-function getFStats(data, sizeVal){
-	$.each(data, function(i, value){
-		console.log("checked: " + i);
-		if(i === sizeVal) {
-			console.log("got: " + value);
-			BMBox.flangeStats = value;
+		if(context.rateSel[0].selectedIndex > 0 && context.sizeSel[0].selectedIndex > 0) {
+			context.sendBut.button("enable");
+		} else {
+			context.sendBut.button("disable");
 		}
-	});
-	getStudStats(BMBox.flangeStats[0]);
-	
-	displayFlange();
-}
-
-function getStudStats(studSize){
-	$.each(BMBox.data.studSizes, function(i, value) {
-		if(i === studSize) {
-			console.log("got stud: " + value);
-			BMBox.studStats = value;
+		context.sendBut.button("refresh");
+		
+		if(context.studSel[0].selectedIndex > 0) {
+			context.studBut.button("enable");
+		} else {
+			context.studBut.button("disable");
 		}
-	});
-	//console.log("Should still be: " + BMBox.torqueStats);
-}
+		context.studBut.button("refresh");
+	};
 	
-function displayFlange() {
+	context.calcStud = function () {
+		studVal = context.studSel[0].selectedIndex;	
+		console.log("studval: " + studVal);
+		context.getStudStatsOrdered(studVal - 1);
+		context.displayStud();
+	};
+	
+	context.getStudStatsOrdered = function(studSize) {
+		context.studStats = context.studStatsOrdered[studSize][0];
+		console.log(context.studStats);
+	};
+	
+	context.displayStud = function(){
+		studStats = context.studStats;
+		studSize = context.studSel.val();
+		studString = ("Stud Size: " + studSize + "\"");
+		toolString = ("Wrench: " + studStats[0] + "\",   Drift Pin: " + studStats[1] + "\"");
+		torqueString = ("B7 Torque: " + studStats[3] + " ft-lbs \nB7M Torque: " + studStats[2] + " ft-lbs");
+		
+		outString = (studString + "\n" +
+			toolString + "\n" +
+			torqueString);
+		
+		alert(outString);
+	};
+	
+	context.calcFlange = function() {
+		sSize = context.sizeSel;
+		sRate = context.rateSel;
+	
+		rateInd = sRate[0].selectedIndex;
+		context.rateVal = sRate.val();
+		context.sizeVal = sizeVal = $(sSize).val();
+		
+		//Inside the $.each() callback above, you would do $.each(this.subaction, function() { alert(this.name); });, that would give you A, B, C, etc. - test it out here:
+		var fStats;
+		switch(rateInd) {
+			case 0:
+				fStats = context.data.fStats150;
+				break;
+	        case 1:
+				fStats = context.data.fStats300;
+				break;
+			case 2:
+				fStats = context.data.fStats400;
+				break;
+		    case 3:
+				fStats = context.data.fStats600;
+				break;
+		    case 4:
+				fStats = context.data.fStats900;
+				break;
+			case 5:
+				fStats = context.data.fStats1500;
+				break;
+		}	
+		context.getFStats(fStats, sizeVal);
+	};
+	
+	context.getFStats = function (data, sizeVal){
+		$.each(data, function(i, value){
+			console.log("checked: " + i);
+			if(i === sizeVal) {
+				console.log("got: " + value);
+				context.flangeStats = value;
+			}
+		});
+		context.getStudStats(context.flangeStats[0]);
+		
+		context.displayFlange();
+	};
+	
+	context.getStudStats = function (studSize){
+		$.each(context.data.studSizes, function(i, value) {
+			if(i === studSize) {
+				console.log("got stud: " + value);
+				context.studStats = value;
+			}
+		});
+		//console.log("Should still be: " + context.torqueStats);
+	};
+	
+	context.displayFlange = function() {
 	//studStats = studSize: [wrench size, drift pin, b7m torque, b7 torque]
 	//flangeStats = flangeSize: [studSize, studIndex, studCount, studLength] 
-	flangeStats = BMBox.flangeStats;
-	studStats = BMBox.studStats;
+	flangeStats = context.flangeStats;
+	studStats = context.studStats;
 	
-	flangeString = ("Flange Size: " + BMBox.sizeVal + "\": " + BMBox.rateVal + "#");
+	flangeString = ("Flange Size: " + context.sizeVal + "\": " + context.rateVal + "#");
 	studString = ("Studs: " + flangeStats[2] + " @ " + flangeStats[0] + "\" x " + flangeStats[2] + "\"");
 	toolString = ("Wrench: " + studStats[0] + "\",   Drift Pin: " + studStats[1] + "\"");
 	torqueString = ("B7 Torque: " + studStats[3] + " ft-lbs \nB7M Torque: " + studStats[2] + " ft-lbs");
@@ -198,4 +192,18 @@ function displayFlange() {
 		studString + "\n"
 		+ toolString + "\n"
 		+ torqueString);
-}
+	};
+	
+})(flangeBox);
+
+//going to make the last module added call the load functions on each.
+$(document).ready(function(){
+	flangeBox.startLoad();	
+});
+
+			
+
+
+
+	
+
